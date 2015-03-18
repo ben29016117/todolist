aafe95ee99fc6c1d8c1c4ad95cfc64d358751534
ben29016117 <a0936727522@gmail.com>
2015-03-18 15:15:49 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,45 @@
+jsonp_handler({
+ "version": "1",
+ "build": 36,
+ "title": "todolist",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/ben29016117/todolist/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css"
+ ],
+ "filesizes": [
+  547,
+  117,
+  652507,
+  97545,
+  277980,
+  778,
+  35607,
+  113502
+ ],
+ "filedates": [
+  "2015-03-17T06:15:42.000Z",
+  "2015-03-17T07:33:32.000Z",
+  "2015-03-17T06:15:42.000Z",
+  "2015-03-18T07:15:00.899Z",
+  "2015-03-17T06:15:42.000Z",
+  "2015-03-18T07:11:17.829Z",
+  "2015-03-17T06:15:42.000Z",
+  "2015-03-17T06:15:42.000Z"
+ ],
+ "date": "2015-03-18T07:15:01.700Z"
+})
